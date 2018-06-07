066a821c8fd956ddbd6f7a3d907a01527ca2f17c
Filipe Frozza <filipefrozza@hotmail.com>
2018-06-07 15:36:42 -0300

    Adicionado o funcionamento de salas
    Adicionado as funções de criação de salas e etc

--- app/pagina/salas/controller.js
+++ app/pagina/salas/controller.js
@@ -38,10 +38,71 @@ angular.module('tablord')
 			$scope.aplicar();
 		};
 
+        socket.on('forcar_entrada', function(m){
+            m = JSON.parse(m);
+            if(m.erro){
+                alert(m.erro);
+            }else{
+                $scope.selecionarSala(m,$scope.you);
+                $scope.aplicar();
+            }
+        });
+
+        $scope.selecionarSala = function(s, you){
+            console.log(s);
+            socket.emit('sala_conectar', JSON.stringify({sala: s.index, login: you.login}));
+        };
+
+        socket.on('sala_conectado', function(m){
+            m = JSON.parse(m);
+            console.log(m);
+            if(m.erro){
+                alert(m.erro);
+            }else{
+                $scope.selSala = m;
+                $scope.selSala.mensagens = [];
+                $scope.aplicar();
+            }
+        });
+
+        socket.on('sala_mensagem_atualizar', function(m){
+            console.log(m);
+            if(m != 'false'){
+                $scope.selSala.mensagens.push(JSON.parse(m));
+                $scope.aplicar();
+            }else{
+                alert('deu merda');
+            }
+        });
+
+        $scope.enviarMensagem = function(m, you, sala){
+            // console.log($scope.mensagem);
+            delete $scope.mensagem;
+            socket.emit('sala_mensagem', JSON.stringify({sala: sala.index, mensagem:{player: you.login, mensagem: m}}));
+            socket.on('sala_mensagem', function(m){
+                if(m=='false'){
+                    alert('não foi possível enviar mensagem');
+                }
+            });
+            $scope.aplicar();
+        };
+
+        $scope.sairSala = function(s, you){
+            socket.emit('sala_sair', JSON.stringify({sala:s.index, player:you.login}));
+        };
+
+        socket.on('sala_saiu' , function(m){
+            delete $scope.you.sala;
+            delete $scope.selSala;
+            socket.emit('buscar_salas');
+            $scope.aplicar();
+        });
+
 		socket.on('atualizar_salas', function(salas){
 			salas = JSON.parse(salas);
 
 			$scope.salas = salas;
+            $scope.aplicar();
 		});
 
 		$scope.aplicar = function(){
--- node/modulos/salas/index.js
+++ node/modulos/salas/index.js
@@ -9,7 +9,7 @@ exports.data = {
 };
 
 exports.criarSala = function(sala, player){
-	var index = nome.toLowerCase().replace(' ','-');
+	var index = sala.nome.toLowerCase().replace(' ','-');
 	if(this.data[index])
 		return false;
 	this.data[index] = {
@@ -17,11 +17,11 @@ exports.criarSala = function(sala, player){
 		integrantes: {},
 		index: index,
 		lider: player.nome,
-		quantidade: 1,
+		quantidade: 0,
 		regras: sala.regras,
 		limite: sala.limite
 	};
-	this.data[index].integrantes[player.index] = player;
+	// this.data[index].integrantes[player.index] = player;
 	return true;
 };
 
@@ -30,36 +30,38 @@ exports.iniciar = function(socket, player){
 		m = JSON.parse(m);
 	    if(!players[m.login]){
 			players[m.login] = {nome: m.nome, sala: null, index: m.login};
-	    	socket.emit('conectado','true');
 	    	// io.emit('conectado',m);
 	    	console.log(m.nome, " se conectou");
-	    }else{
-	    	io.emit('conectado','false');
-	    	console.log(m.nome, " conexao duplicada");
 	    }
+	    socket.emit('conectado','true');
 	});
 
 	socket.on('sala_conectar',function(m){
 		m = JSON.parse(m);
 	    console.log(m.login, "tentando se conectar a sala "+m.sala);
-	    if(salas.data[m.sala].quantidade >= 4){
-	    	socket.emit('sala_conectado', JSON.stringify({erro: "Sala lotada"}));
-	    	console.log(m.login, ", a sala estava lotada");
-	    }else if(salas.data[m.sala].integrantes[m.login]){
-	    	socket.emit('sala_conectado', JSON.stringify({erro: "Já está nessa sala"}));
-	    	console.log(m.login, "já estava conectado a sala");
-	    }else{
-	      	salas.data[m.sala].integrantes[m.login] = m;
-	      	salas.data[m.sala].quantidade++;
-	      	// salas['iniciantes'].mensagens.push();
-	      	socket.join(m.sala);
-	      	io.to(m.sala).emit('sala_conectou', JSON.stringify(salas.data[m.sala].integrantes));
-	      	io.emit('sala_alterada','');
-	      	// rsala = Object.assign({}, salas.data[m.sala]);
-	      	// delete rsala.mensagens;
-	      	socket.emit('sala_conectado', JSON.stringify(salas.data[m.sala]));
-	      	io.to(m.sala).emit('sala_mensagem_atualizar', JSON.stringify({index: 'Sistema', mensagem: m.login+" entrou na sala"}));
-	    }
+	    if(salas.data[m.sala]){
+		    if(salas.data[m.sala].quantidade >= 4){
+		    	socket.emit('sala_conectado', JSON.stringify({erro: "Sala lotada"}));
+		    	console.log(m.login, ", a sala estava lotada");
+		    }else if(salas.data[m.sala].integrantes[m.login]){
+		    	socket.emit('sala_conectado', JSON.stringify({erro: "Já está nessa sala"}));
+		    	console.log(m.login, "já estava conectado a sala");
+		    }else{
+		      	salas.data[m.sala].integrantes[m.login] = m;
+		      	salas.data[m.sala].quantidade++;
+		      	// salas['iniciantes'].mensagens.push();
+		      	socket.join(m.sala);
+		      	io.to(m.sala).emit('sala_conectou', JSON.stringify(salas.data[m.sala].integrantes));
+		      	io.emit('sala_alterada','');
+		      	// rsala = Object.assign({}, salas.data[m.sala]);
+		      	// delete rsala.mensagens;
+		      	socket.emit('sala_conectado', JSON.stringify(salas.data[m.sala]));
+		      	io.emit('atualizar_salas', JSON.stringify(salas.data));
+		      	io.to(m.sala).emit('sala_mensagem_atualizar', JSON.stringify({player: 'Sistema', mensagem: m.login+" entrou na sala"}));
+		    }
+		}else{
+			socket.emit('sala_conectado', JSON.stringify({erro: "A sala não existe mais"}));
+		}
 	});
 
 	socket.on('buscar_salas', function(m){
@@ -104,7 +106,7 @@ exports.iniciar = function(socket, player){
 
 	socket.on('criar_sala', function(m){
 		m = JSON.parse(m);
-		var index = m.sala.toLowerCase().replace(' ','-');
+		var index = m.nome.toLowerCase().replace(' ','-');
 		if(salas.criarSala(m, player)){
 			io.emit('sala_criada', JSON.stringify(salas.data));
 			socket.emit('forcar_entrada', JSON.stringify(salas.data[index]));
